11789f8496756f077abdf6d730336bbfee0f3cb1
Xue Liangliang <xueliangliang@hotmail.com>
2020-11-30 00:35:34 +0800

    add graful shutdown logic

--- Week02/cmd/main.go
+++ Week02/cmd/main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"xll.com/go-000/Week02/cmd/handler"
 	"xll.com/go-000/Week02/internal/service"
@@ -19,5 +25,35 @@ func main() {
 		log.Fatalf("Cannot initialize userService due to: %v", err)
 	}
 	r.GET("/user/:id", handler.GetUserHander(userService))
-	r.Run()
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+	// Initializing the server in a goroutine so that
+	// it won't block the graceful shutdown handling below
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
+	log.Println("Server exiting")
 }
